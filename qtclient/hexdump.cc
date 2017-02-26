fd2c236dc9a572e12e557771bbe40fc63a3823b6
Peter Hercek <phercek@gmail.com>
2017-02-26 01:47:54 +0100

    do not hexdump a repeating byte at a message end

--- qtclient/hexdump.cc
+++ qtclient/hexdump.cc
@@ -8,6 +8,14 @@ static char *append(char *s1, const char *s2) {
 	return s1 + strlen(s2);
 }
 
+static bool isOnlyARepeatingByteOutstanding(uint8_t const* data, size_t len) {
+	uint8_t const byte = *data;
+	do {
+		++data; --len;
+	} while (len && *data == byte);
+	return 0 == len;
+}
+
 char *hexdump::formatLine(char line[80], size_t *offset) const {
 	char* cursor = &line[0];
 	const int bytesPerChunk = 16;
@@ -15,6 +23,12 @@ char *hexdump::formatLine(char line[80], size_t *offset) const {
 	const uint8_t *base = &mData[*offset];
 
 	cursor += sprintf(cursor, "%08x  ", (int32_t) *offset);
+
+	if (isOnlyARepeatingByteOutstanding( base, mLen-*offset )) {
+		sprintf(cursor, "repeating 0x%02X up to the end offset of 0x%X", *base, (uint32_t)mLen);
+		*offset = mLen;
+		return &line[0];
+	}
 
 	int i;
 	for(i = 0; i < chunkSize && i < (bytesPerChunk / 2); ++i){
