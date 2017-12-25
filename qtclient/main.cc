4665a97a35b39a4898dcfce4e515c2a961b6017b
Peter Hercek <phercek@gmail.com>
2017-12-25 09:35:42 +0100

    support for copy/paste instalation on windows

--- qtclient/main.cc
+++ qtclient/main.cc
@@ -3,6 +3,19 @@
 #include "libusb.h"
 
 int main(int argc, char **argv) {
+#if defined(Q_OS_WIN)
+	{
+	HMODULE hModule = GetModuleHandleW(NULL);
+	WCHAR path[MAX_PATH];
+	GetModuleFileNameW(hModule, path, MAX_PATH);
+	QString pluginDir( QString::fromWCharArray(path) );
+	int pos = pluginDir.lastIndexOf( '\\' );
+	pluginDir.truncate( pos );
+	pluginDir += "\\plugins";
+	QCoreApplication::addLibraryPath(pluginDir);
+	}
+#endif
+
 	QApplication app(argc, argv);
 
 	libusb_init(NULL);
